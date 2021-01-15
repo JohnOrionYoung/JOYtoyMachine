d0bb31aae109ed484f169573da75d296bfa755e7
Trislit <trislit@hotmail.com>
2021-01-15 18:34:25 -0500

    Update Product.vue

--- components/Product.vue
+++ components/Product.vue
@@ -16,24 +16,12 @@
           <h4>{{ tokenData.title }}</h4>
           <p class="description small">{{ tokenData.feature }}</p>
           <p class="description small">{{ tokenData.description }}</p>
-          //<div class="metaRow">
-          //  <span class="metaLabel">ID</span>
-          //  <span class="metaValue">{{ tokenData.id }}</span>
-          //</div>
+          
+          </div>
           <div class="metaRow">
             <span class="metaLabel">Total</span>
             <span class="metaValue">{{ tokenData.editionSize }}</span>
           </div>
-          //<div class="metaRow">
-<<<<<<< Updated upstream
-           // <span class="metaLabel">Number</span>
-           // <span class="metaValue">{{ tokenData.editionNumber }}</span>
-         // </div>
-=======
-          //  <span class="metaLabel">Number</span>
-           // <span class="metaValue">{{ tokenData.editionNumber }}</span>
-          //</div>
->>>>>>> Stashed changes
           <div class="metaRow">
             <span class="metaLabel">Price</span>
             <span class="metaValue">{{ tokenData.price }} Eth</span>

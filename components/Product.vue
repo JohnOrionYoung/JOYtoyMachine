d0b7c471db34180f70bac4ce09b521146513f0ca
Trislit <trislit@hotmail.com>
2021-01-15 19:32:30 -0500

    Update Product.vue

--- components/Product.vue
+++ components/Product.vue
@@ -17,10 +17,14 @@
           <p class="description small">{{ tokenData.feature }}</p>
           <p class="description small">{{ tokenData.description }}</p>
           <div class="metaRow">
+           
           </div>
           <div class="metaRow">
             <span class="metaLabel">Total</span>
             <span class="metaValue">{{ tokenData.editionSize }}</span>
+          </div>
+          <div class="metaRow">
+           
           </div>
           <div class="metaRow">
             <span class="metaLabel">Price</span>

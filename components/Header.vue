8771e80529a3e5524e78af3acee9da0a3a7c17fe
John Orion Young <13425893+JohnOrionYoung@users.noreply.github.com>
2021-01-21 22:03:07 -0800

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,81 +1,9 @@
 <template>
   <header class="Header">
-    <div class="row full between">
       <Brand />
-    </div>
-    
-    
+    </div>    
   </header>
 </template>
-
-<script>
-import { mapMutations, mapGetters, mapActions } from "vuex"
-import tokenshop from "../tokenshop.config"
-
-export default {
-  data() {
-    return {
-      brandName: "",
-      rootUrl: "",
-      requiredNetwork: "",
-
-      VERCEL_ENV: "",
-      VERCEL_GIT_REPO_SLUG: "",
-      VERCEL_GIT_COMMIT_SHA: "",
-      VERCEL_GIT_COMMIT_MESSAGE: "",
-      VERCEL_URL: ""
-    }
-  },
-  computed: {
-    ...mapGetters({
-      walletStatus: "walletStore/walletStatus",
-      walletAddress: "walletStore/walletAddress",
-      walletNetwork: "walletStore/walletNetwork",
-      walletType: "walletStore/walletType"
-    })
-  },
-  created() {
-    this.brandName = tokenshop.site.brandName
-    this.hasNavigation = tokenshop.site.hasNavigation
-  },
-
-  mounted() {
-    // these are environmentVariables imported via NUXT config
-    if (this.$config) {
-      // const requiredNetwork = this.$config.requiredNetwork
-      // const VERCEL_ENV = this.$config.VERCEL_ENV || 'local'
-      // const VERCEL_GIT_REPO_SLUG = this.$config.VERCEL_GIT_REPO_SLUG || 'local'
-      // const VERCEL_GIT_COMMIT_SHA =
-      //   this.$config.VERCEL_GIT_COMMIT_SHA || 'local'
-      // const VERCEL_GIT_COMMIT_MESSAGE =
-      //   this.$config.VERCEL_GIT_COMMIT_MESSAGE || '-'
-    }
-    // if (process.client) {
-    // checks if the wallet is on the cirrect network
-    const requiredNetwork = this.$config.requiredNetwork
-    this.networkCheck(requiredNetwork)
-    // }
-  },
-
-  methods: {
-    ...mapMutations({
-      setWalletNetwork: "walletStore/setWalletNetwork"
-    }),
-
-    ...mapActions({
-      handleWeb3Connect: "walletStore/handleWeb3Connect",
-      networkCheck: "walletStore/networkCheck"
-    }),
-    handleConnect() {
-      console.log("this", this)
-      const web3Modal = this.$web3Modal
-      if (web3Modal) {
-        this.handleWeb3Connect(web3Modal)
-      }
-    }
-  }
-}
-</script>
 
 <style lang="scss">
 .Header {

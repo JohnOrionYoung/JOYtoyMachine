7b05953633ccddf75e197be83b4aac7703723114
Kevin <thekidnamedkd@gmail.com>
2021-03-26 08:43:04 -0700

    refactor web3Modal

--- plugins/web3ModalPlugin.js
+++ plugins/web3ModalPlugin.js
@@ -1,20 +1,10 @@
 import Web3Modal from "web3modal"
-// import Portis from '@portis/web3'
 import WalletConnectProvider from "@walletconnect/web3-provider"
-// import tokenshop from "./../tokenshop.config"
 
 let web3Modal
-const requiredNetwork =
-  process.env.PROD_NETWORK ||
-  process.env.STAGING_NETWORK ||
-  process.env.DEV_NETWORK ||
-  "rinkeby"
 
 function init() {
-  let web3ModalNetwork = "mainnet"
-  if (requiredNetwork !== "main") {
-    web3ModalNetwork = "rinkeby"
-  }
+  const web3ModalNetwork = "mainnet"
 
   const providerOptions = {
     walletconnect: {
@@ -32,7 +22,6 @@ function init() {
     disableInjectedProvider: false // optional. For MetaMask / Brave / Opera.
   })
 }
-
 export default async ({ app, store, $axios, isHMR }, inject) => {
   if (process.client) {
     await init()

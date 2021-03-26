ae8db9d7bcd122dc1125efd2f125c71544fea2a8
Kevin <thekidnamedkd@gmail.com>
2021-03-26 16:38:28 -0700

    test: enable Vercel workaround second attempt

--- plugins/web3plugin.js
+++ plugins/web3plugin.js
@@ -1,30 +1,32 @@
-// import { initWeb3 } from "../utils/wallet"
-// import tokenshop from "../tokenshop.config"
+import { initWeb3 } from "../utils/wallet"
 
-// const web3Override = require("web3")
+function init(requiredNetwork) {
+  window.addEventListener("load", function () {
+    if (window.ethereum) {
+      // check web3js where the script added it to window
+      // this is an annoying workaround.
 
-// function init() {
-//   window.addEventListener("load", function () {
-//     console.log("network", window.ethereum)
-//     if (window.ethereum) {
-//       // check web3js where the script added it to window
-//       // this is an annoying workaround.
-//       const infuraUrl = tokenshop.keys.infura.main
-//       const Web3 = web3Override
-//       window.web3 = undefined
-//       const web3Write = new Web3(new Web3.providers.HttpProvider(infuraUrl))
-//       window.web3Write = web3Write
-//     }
+      const Web3 = window.Web3
+      window.web3 = undefined
+      const web3Write = new Web3(window.ethereum)
+      window.web3Write = web3Write
+    }
 
-//     // this will init the web3read using default (main) infura
-//     // if it doesnt' already exist
+    // this will init the web3read using default (main) infura
+    // if it doesnt' already exist
 
-//     if (!window.web3Read) {
-//       initWeb3()
-//     }
-//   })
-// }
+    if (!window.web3Read) {
+      initWeb3()
+    }
+  })
+}
 
-// export default async () => {
-//   await init()
-// }
+export default async ({ app, store, $axios, isHMR }, inject) => {
+  if (process.client) {
+    const { requiredNetwork } = app.$config
+    // web3 workaround for vercel deployments
+    // init function waits for page load to ensure script
+    // exists, since we cannot use node module
+    await init(requiredNetwork)
+  }
+}
